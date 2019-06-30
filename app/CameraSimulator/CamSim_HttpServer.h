edb606f1365ae9b0a7fd6bb72b1da6c3da40fb66
blackphil <github@schwarz-philipp.net>
2019-06-30 15:42:19 +0200

    Prepared the new CameraSimulator tool.

---
+++ app/CameraSimulator/CamSim_HttpServer.h
@@ -0,0 +1,40 @@
+#ifndef CAMSIM_HTTPSERVER_H
+#define CAMSIM_HTTPSERVER_H
+
+#include <QObject>
+#include <QTcpServer>
+#include <QThread>
+
+
+namespace CamSim {
+
+class HttpConnection : public QObject
+{
+    Q_OBJECT
+
+    QTcpSocket* socket;
+    QThread* workerThread;
+
+public :
+    HttpConnection(QTcpSocket* socket, QObject* parent = nullptr);
+};
+
+class HttpServer : public QTcpServer
+{
+    Q_OBJECT
+
+    QList<HttpConnection*> connections;
+
+public:
+    HttpServer(QHostAddress addr, quint16 port, QObject* parent = nullptr);
+
+private Q_SLOTS :
+    void handleNewConnection();
+
+protected:
+    void run();
+};
+
+} // namespace CamSim
+
+#endif // CAMSIM_HTTPSERVER_H
